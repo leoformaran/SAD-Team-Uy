5dd3e25a02c6cc729830edd6a804926f842a4c5b
kenlayug <ken_layug@yahoo.com>
2015-12-07 20:15:24 +0800

    Added model class 'Name'.

---
+++ practice-web/src/com/practiceweb/model/Name.java
@@ -0,0 +1,30 @@
+package com.practiceweb.model;
+
+public class Name {
+
+	private String strFirstName;
+	private String strMiddleName;
+	private String strLastName;
+	
+	public String getStrFirstName() {
+		return strFirstName;
+	}
+	public void setStrFirstName(String strFirstName) {
+		this.strFirstName = strFirstName;
+	}
+	public String getStrMiddleName() {
+		return strMiddleName;
+	}
+	public void setStrMiddleName(String strMiddleName) {
+		this.strMiddleName = strMiddleName;
+	}
+	public String getStrLastName() {
+		return strLastName;
+	}
+	public void setStrLastName(String strLastName) {
+		this.strLastName = strLastName;
+	}
+	
+	
+	
+}
